e04a6004090483acbedb54ef8671d8e6ef3cbfae
2bTwist <ndanjiedmond@gmail.com>
2025-10-17 04:06:25 -0400

    fix: update babel configuration and import order in Screen component

--- mobile/babel.config.js
+++ mobile/babel.config.js
@@ -2,7 +2,11 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo"],
+      'babel-preset-expo',
+      'react-native-css-interop/babel',
     ],
-    };
+    plugins: [
+      'react-native-reanimated/plugin', // must be last
+    ],
+  };
 };
